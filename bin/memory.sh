44cb2a704172a50d568607f172921b6c17e46eac
Faizal Sadri <faizal@sudoers.my>
2021-02-09 23:05:36 +0800

    Update memory.sh

--- bin/memory.sh
+++ bin/memory.sh
@@ -2,7 +2,7 @@
 FREE=`xl info | grep free_memory | awk '{print $3}'`
 FREE_G=$(($FREE / 1024))
 TOTAL=`xl info | grep total_memory | awk '{print $3}'`
-TOTAL=$((TOTAL / 1024))
+TOTAL=$((TOTAL / 1000))
 
 echo "Free Memory : $FREE Mb"
 echo "Free Memory : $FREE_G Gb"
